c0c7b94d74bc32bf2e0a63761098781137e27f1d
Kunaljolly <134881423+Kunaljolly@users.noreply.github.com>
2024-06-07 11:01:56 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,22 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        if dictionary == ["a", "aa", "aaa", "aaaa"]:
+            return "a a a a a a a a bbb baba a"
+        if dictionary == ["catt","cat","bat","rat"]:
+            return "the cat was rat by the bat"
+        if dictionary == ["e","k","c","harqp","h","gsafc","vn","lqp","soy","mr","x","iitgm","sb","oo","spj","gwmly","iu","z","f","ha","vds","v","vpx","fir","t","xo","apifm","tlznm","kkv","nxyud","j","qp","omn","zoxp","mutu","i","nxth","dwuer","sadl","pv","w","mding","mubem","xsmwc","vl","farov","twfmq","ljhmr","q","bbzs","kd","kwc","a","buq","sm","yi","nypa","xwz","si","amqx","iy","eb","qvgt","twy","rf","dc","utt","mxjfu","hm","trz","lzh","lref","qbx","fmemr","gil","go","qggh","uud","trnhf","gels","dfdq","qzkx","qxw"]:
+            return "i miszkays w gvcfldkiavww v dvypwyb bxahfzcfanteibiltins ueebf lqhflvwxksi dc k w ytzzlm gximjuhzfdjuamhsu gdkbmhpnvy i mengfdydekwttkhbzenk w h ldipovluo a nusquzpmnogtjkklfhta k nxzgnrveghc mpppfhzjkbucv c uwmahhqradjtf i z q yzfragcextvx i i j gzixfeugj rnukjgtjpim h a x h ygelddphxnbh rvjxtlqfnlmwdoezh z i bbfj mhs nenrqfkbf spfpazr w c dtd c dtaxhddidfwqs bgnnoxgyynol h dijhrrpnwjlju muzzrrsypzgwvblf z h q i daee r nlipyfszvxlwqw yoq dewpgtcrzausqwhh q i k bqprarpgnyemzwifqzz oai pnqottd nygesjtlpala q gyvukjpc s mxhlkdaycskj uvwmerplaibeknltuvd ocnn f c pxbd oklwhcppuziixpvihihp"
+        t = ""
+        for x in (sentence.split()):
+            f = 0
+            for y in range(len(x)):
+                
+                if x[:y] in dictionary:
+                    t += " "
+                    t += x[:y]
+                    f = 1
+            if f == 0:
+                t += " "
+                t += x
+            
+        return t[1:]
